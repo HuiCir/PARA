9bfd2d01787a3d6953de2603368d01beb54cb6f6
Migraine <45650630+HuiCir@users.noreply.github.com>
2025-05-20 13:28:49 +0800

    Update para.py

--- raglab/rag/infer_alg/para/para.py
+++ raglab/rag/infer_alg/para/para.py
@@ -63,15 +63,51 @@ class para(NaiveRag):
                 outputs_text = Outputs_o.text
         generation_track['final answer'] = outputs_text
         return outputs_text, generation_track
-    
+        
+    def bm25(self, query, corpus, n=30):
+        # Tokenize the corpus
+        #print(len(corpus))
+        tokenized_corpus = [str(doc).split(" ") for doc in corpus]
+
+        # Initialize the BM25 model
+        bm25 = BM25Okapi(tokenized_corpus)
+
+        # Define a query
+
+        tokenized_query = query.split(" ")
+
+        # Get document scores
+        doc_scores = bm25.get_scores(tokenized_query)
+        #print(doc_scores)
+
+        # Retrieve the top document
+        top_doc = bm25.get_top_n(tokenized_query, corpus, n=n)
+        #print('\n top_doc:',top_doc)
+        return top_doc
+    def rrf(self, dlist, slist, lamada=0.8):
+        rrflist = []
+        for i in range(len(slist)):
+            didx = dlist.index(slist[i])
+            rrf_score = 1/(didx + lamada*i)
+            rrflist.append(rrf_score)
+
+        sort_zip = zip(slist,rrflist)
+
+        sorted_zip = sorted(sort_zip,key=lambda x:x[1])
+ 
+        result = zip(*sorted_zip)
+        slist, rrflist = [list(x) for x in result]
+        return slist
     def chunk(self,query, evidence, oprob ,t=3):
         evidence_passage = ""
         eprob = oprob
         Doc = self.nlp(evidence)
         sentlist = list(Doc.sents)
-        print(len(sentlist))
+        #print(len(sentlist))
         k = self.parak
         m = self.d
+        top_chunk = self.bm25(query, sentlist)
+        sentlist = self.rrf(sentlist, top_chunk)
         for i in range(len(sentlist)):
             if k == 0 or eprob > self.ptheta:
                 break
